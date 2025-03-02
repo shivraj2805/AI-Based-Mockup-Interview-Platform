4bba33817cc321b1b967188a2ea3ebe389ea317c
shivraj2805 <darekar1005@gmail.com>
2025-03-02 20:45:42 +0530

    Added br tag in between heading

--- components/hero.jsx
+++ components/hero.jsx
@@ -32,7 +32,7 @@ function HeroSection() {
     <div className="pb-20 px-4">
         <div className="container mx-auto text-center">
             <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title" >
-               AI Mock Interview Platform <br/> 
+               AI Mock Interview <br/> Platform <br/> 
             </h1>
             <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
             Advance your career with personalized guidance and AI-powered interview preparation for job success.
